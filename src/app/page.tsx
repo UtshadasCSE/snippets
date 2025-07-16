0c3b8f19ae1b8e50dc93cf4edd7bba73ca925846
UtshadasCSE <utshachandradas@gmail.com>
2025-07-16 15:34:13 +0600

    Snippets project started

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <h1>Snippets</h1>
+        <Button>New</Button>
+      </div>
+    </div>
+  );
+}
